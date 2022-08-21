1f002217d7d8ddab7ff0aef89c5424101e7b6dae
vonunwerth <maximilian@von-unwerth.de>
2022-08-21 02:55:19 +0200

    Dynamic visualization of meshes

---
+++ README.md
@@ -0,0 +1,15 @@
+# RViz Object Visualizer 
+Just launch the ```display.launch``` file to visualize all meshes from the *meshes* folder and visualize them as markers in RViz
+
+### Licence
+The RViz Object Visualizer source is released under the BSD License.
+
+### Copyright
+```
+RViz Object Visualizer
+
+Maximilian von Unwerth
+von-unwerth.de
+Copyright(C) 2022
+All rights reserved.
+```
---
+++ scripts/object_visualizer.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python3
+import os
+import rospy
+import rospkg
+from visualization_msgs.msg import MarkerArray, Marker
+
+rospy.init_node("object_visualizer")
+rate = rospy.Rate(1)
+rospy.loginfo('Initializing object visualizer')
+rp = rospkg.RosPack()
+visualizer_path = os.path.join(rp.get_path('object_visualizer'), 'meshes')
+markerArray = MarkerArray()
+
+publisher = rospy.Publisher('visualization_marker', MarkerArray, queue_size=1)
+
+current_file_count = 0
+while not rospy.is_shutdown():
+    files = [file for file in os.listdir(visualizer_path) if
+             file.endswith(('.dae', '.stl', '.mesh'))]  # find all meshes in the 'meshes' folder
+    if len(files) != current_file_count:  # if the number of valid meshed in the 'meshes' folder has changed
+        if len(files) < current_file_count:  # if some markers are removed from the 'meshes' folder, delete them in RViz
+            marker = Marker()
+            marker.header.frame_id = 'map'
+            marker.action = marker.DELETEALL
+            markerArray.markers.append(marker)
+            publisher.publish(markerArray)
+
+        current_file_count = len(files)
+        for marker_id, file in enumerate(files):
+            rospy.loginfo('Loading file: %s', file)
+            marker = Marker()
+            marker.id = marker_id
+            marker.mesh_resource = 'package://object_visualizer/meshes/' + file
+            marker.mesh_use_embedded_materials = True  # Need this to use textures for mesh
+            marker.type = marker.MESH_RESOURCE
+            marker.header.frame_id = "map"
+            marker.scale.x = 1.0
+            marker.scale.y = 1.0
+            marker.scale.z = 1.0
+            marker.pose.orientation.w = 1.0
+            markerArray.markers.append(marker)
+
+    rospy.loginfo('Published %d objects. ', len(markerArray.markers))
+    publisher.publish(markerArray)
+    rate.sleep()
